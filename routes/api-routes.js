1f47d11a3a32f2f9b3cf51b8d197a5c474521e7c
Zan Hong <mokzanhong92@hotmail.com>
2020-09-12 16:55:30 +0930

    Added GET workout in range route and added {new: true} to ADD exercise route

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -15,14 +15,16 @@ module.exports = app => {
 
   // Add exercise
   app.put("/api/workouts/:id", (req, res) => {
-    console.log(req.params.id);
-    console.log(req.body);
-    console.log(req.body.duration);
+    // console.log(req.params.id);
+    // console.log(req.body);
+    // console.log(req.body.duration);
     db.Workout.findByIdAndUpdate({
       _id: req.params.id
     }, {
       $push: { exercises: req.body },
       $inc: { totalDuration: req.body.duration }
+    }, {
+      new: true
     })
       .then(dbWorkout => {
         console.log(dbWorkout);
@@ -37,10 +39,22 @@ module.exports = app => {
   app.post("/api/workouts", (req, res) => {
     db.Workout.create(req.body)
       .then(dbWorkout => {
+        // console.log(dbWorkout);
         res.json(dbWorkout);
       })
       .catch(err => {
         res.status(400).json(err);
+      });
+  });
+
+  // Get workout in range
+  app.get("/api/workouts/range", (req, res) => {
+    db.Workout.find({})
+      .then(dbWorkout => {
+        res.json(dbWorkout)
+      })
+      .catch(err => {
+        res.status(400).json(err)
       });
   });
 }
