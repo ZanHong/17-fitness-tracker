63a4c8dc8d9babd9c1bbee40b207611905e4ec51
Zan Hong <mokzanhong92@hotmail.com>
2020-09-12 16:05:08 +0930

    Added CREATE workout route

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -34,13 +34,13 @@ module.exports = app => {
   });
 
   // Create workout
-  // app.post("/api/workout", (req, res) => {
-  //   db.Workout.create({})
-  //     .then(dbWorkout => {
-  //       res.json(dbWorkout);
-  //     })
-  //     .catch(err => {
-  //       res.status(400).json(err);
-  //     });
-  // })
+  app.post("/api/workouts", (req, res) => {
+    db.Workout.create(req.body)
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.status(400).json(err);
+      });
+  });
 }
